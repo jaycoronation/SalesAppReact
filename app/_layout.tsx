bb38717f53bb8109a4dcd54a6801cb864a120761
JatinDabhi24 <jatin@coronation.in>
2026-03-20 12:47:32 +0530

    pratiksha 1st commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// app/_layout.tsx
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
